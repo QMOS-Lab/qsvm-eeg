5b1c23bf697531f1c62c12998c91834b5bb0500a
dduyanhhoang <dduyanhhoang@gmail.com>
2026-01-29 03:20:28 +0700

    feat: run quantum expr upto 2000 samples

---
+++ run_quantum.ps1
@@ -0,0 +1,19 @@
+$sample_sizes = @(3000, 6000, 8000)
+
+$patient_configs = @(
+    "48"
+    # "411",
+    # "48 411"
+)
+
+foreach ($patients in $patient_configs) {
+    foreach ($n in $sample_sizes) {
+        Write-Host "------------------------------------------------"
+        Write-Host "Running Kernel-based QSVR: Patients [$patients] | Samples: $n"
+        Write-Host "------------------------------------------------"
+
+        uv run main.py -p $patients.Split(' ') -n $n -j 1
+    }
+}
+
+Write-Host "All experiments completed!"
